f832deb5ad9d0e87314e0c636619f7831102dbdd
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-07-17 05:23:19 +0700

    Add Home Route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const userRoutes = require('./user')
 const taskRoutes = require('./task')
 
+router.get('/', (req,res) => {
+    res.send("Hello , This Is Rest Api My Kanban Ichlasul")
+})
+
 router.use('/users', userRoutes)
 router.use('/tasks', taskRoutes)
 
