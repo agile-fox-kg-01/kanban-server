d00baa4eee5f9139bc0d7decad1fea3a3fc43428
Nurfiah Idris <nurfiahidris@Nurfiahs-MacBook-Air.local>
2020-07-18 02:12:42 +0800

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorHandler.js')
 const routes = require('./routes/index.js')
 const cors = require('cors')
