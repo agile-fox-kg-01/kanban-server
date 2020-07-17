af67a4f45cbb26da26cc0a9806162070bf8e2755
Nurfiah Idris <nurfiahidris@Nurfiahs-MacBook-Air.local>
2020-07-18 01:46:42 +0800

    deploy

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const errorHandler = require('./middlewares/errorHandler.js')
-const index = require('./routes/index.js')
+const routes = require('./routes/index.js')
 const cors = require('cors')
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
-app.use('/', index)
+app.use('/', routes)
 app.use(errorHandler)
 app.listen(port, () => {
   console.log(`app running on http://localhost:${port}`);
